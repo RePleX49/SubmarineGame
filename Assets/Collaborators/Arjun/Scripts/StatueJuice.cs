be62efb2e08a624713ca50a1bf6de831c77ce831
asj397 <asj397@nyu.edu>
2020-11-27 10:50:02 -0500

    Added Juice to Statues

--- Assets/Collaborators/Arjun/Scripts/StatueJuice.cs
+++ Assets/Collaborators/Arjun/Scripts/StatueJuice.cs
@@ -6,24 +6,27 @@ public class StatueJuice : MonoBehaviour
 {
     public bool isPuzzleComplete = false;
     public Renderer[] statues;
-    public Material baseStatueMaterial;
+    public Material[] baseStatueMaterials;
     public Material[] completeStatueMaterials;
     public float speed = 2f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < statues.Length; i++)
+        {
+            statues[i].material = baseStatueMaterials[i];
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() 
     {
         if (isPuzzleComplete)
         {
             for (int i = 0; i < statues.Length; i++)
             {
-                statues[i].materials[0].Lerp(baseStatueMaterial, completeStatueMaterials[i], Time.deltaTime * speed);
+                statues[i].materials[0].Lerp(statues[i].materials[0], completeStatueMaterials[i], Time.deltaTime * speed);
             }
         }
     }
