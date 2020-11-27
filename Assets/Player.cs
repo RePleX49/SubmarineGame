1b70b0ba212960a380e619323a2660c34ffa5f0b
asj397 <asj397@nyu.edu>
2020-11-27 16:06:54 -0500

    FInished Ending Cutscene

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -155,6 +155,10 @@ public class Player : MonoBehaviour
             alignmentPosition = other.GetComponent<PillarData>().pillarPos;
             alignmentRotation = Quaternion.Euler(other.GetComponent<PillarData>().pillarRot);
         }
+        else if (other.tag == "HolyLight")
+        {
+            other.GetComponent<EndingCutscene>().showWinText = true;
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -163,6 +167,10 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(Systems.UI.FadeOutText(Systems.UI.pillarText, "Press F to Dock", 1f, Color.white));
             dockable = false;
+        }
+        else if (other.tag == "HolyLight")
+        {
+            other.GetComponent<EndingCutscene>().showWinText = false;
         }
     }
 
